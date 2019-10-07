8877bf7efe284efe90c2455ee87e0d24f2a6c219
Eric Rees <erees@protonmail.com>
2019-10-07 11:33:09 -0400

    Routing and Auth Services.

---
+++ src/app/indeed-search/indeed-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indeed-search',
+  templateUrl: './indeed-search.component.html',
+  styleUrls: ['./indeed-search.component.css']
+})
+export class IndeedSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
