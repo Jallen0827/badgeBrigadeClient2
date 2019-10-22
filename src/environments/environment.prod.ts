0f38f6e15a929bfa3e03b46d704dac7afc06372a
Jallen <JustinAllen0827@gmail.com>
2019-10-22 13:41:33 -0400

    readied files for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
   production: true
 };
+
+export let APIURL = '';
+
+switch (window.location.hostname) {
+    // this is the deployed angular application
+    case 'badgebrigade.herokuapp.com':
+        // this is the full url of your deployed API
+        APIURL = 'https://badgebrigade.herokuapp.com';
+        break;
+    default:
+        // this is the local host name of your API
+        APIURL = 'http://localhost:3002';
+}
